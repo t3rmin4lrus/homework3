9b03d09eb0ca7a585e8912a859da9622ee899ff2
t3rmin4alrus <eurosetkazalakov1996@gmail.com>
2021-07-27 20:23:02 +0300

    Добавьте файлы проекта.

---
+++ homework3.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// задание 4 (1)
+
+extern const int a;
+extern const int b;
+extern const int c;
+extern const int d;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x = a * (b + (c / d));
+    cout << "целочисленное выражение: " << x << endl;
+
+    float y = a * (b + static_cast <float> (c) / d);
+    cout << "выражение с плавающей точкой: " << y << endl;
+
+    float z = static_cast <float> (c) / d;
+    cout << "выражение c/d: " << z << endl;
+
+    return 0;
+}
+
+// задание 2
+
+int main1()
+{
+    int ba, bb;
+    cout << "enter the variable ba: ";
+    cin >> ba;
+
+    bb = (ba <= 21) ? ba : -ba;
+    cout << (21 - ba) << endl;
+
+    bb = (ba > 21) ? ba : -ba;
+    cout << (21 - ba) * 2 << endl;
+
+    return 0;
+}
+
+// задание 3
+
+int main2() {
+
+    int arr[3][3][3] = { { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } },
+                       { { 9, 10, 11}, {12, 13, 14 }, { 15, 16, 17 } },
+                       { { 18, 19, 20 }, { 21, 22, 23 }, { 24, 25, 26 } } }, *pArr;
+
+     
+    pArr = &arr[1][1][1];
+
+    cout << *pArr << endl;
+
+    return 0;
+}
---
+++ source.cpp
@@ -0,0 +1,4 @@
+extern const int a = 5;
+extern const int b = 3;
+extern const int c = 8;
+extern const int d = 11;
